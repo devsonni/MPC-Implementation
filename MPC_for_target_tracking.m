76866350815516ddf7ccc9104cdb691d2e9da591
Dev Soni <devsoni44.pp@gmail.com>
2021-09-16 17:23:38 +0530

    just initial befor gimble

--- MPC_for_target_tracking.m
+++ MPC_for_target_tracking.m
@@ -1,5 +1,6 @@
 % This code is for the implementation of the MPC for the target - 
 % tracking (without gimbal and without obstacles scenario) problem using CasADi v3.5.5.
+clear all; clc; close all;
 
 addpath('C:\Users\Dev Soni\Desktop\casadi')
 import casadi.*
@@ -19,30 +20,32 @@ z_u_min = 75; z_u_max = 150;
 
 % States without gimbal camera
 
-x_u = SX.sym('x_u'); y_u = SX.sym('y_u'); z_u = SX.sym('z_u'); theta_u = SX.sym('theta_u'); psi_u = SX.sym('psi_u'); %states of the UAV
-x_t = SX.sym('x_t'); y_t = SX.sym('y_t'); theta_t = SX.sym('theta_t');                                               %states of the target
+x_u = SX.sym('x_u'); y_u = SX.sym('y_u'); z_u = SX.sym('z_u'); theta_u = SX.sym('theta_u'); psi_u = SX.sym('psi_u');   %states of the UAV
+%x_t = SX.sym('x_t'); y_t = SX.sym('y_t'); theta_t = SX.sym('theta_t');                                               %states of the target
 
-states = [x_u; y_u; z_u; theta_u; psi_u; x_t; y_t; theta_t];      n_states = length(states);
+states_u = [x_u; y_u; z_u; theta_u; psi_u];      n_states_u = length(states_u); %UAV
+%states_t = [x_t; y_t; theta_t];    n_states_t = length(states_t); %target
 
 % Controls
 
-v_u = SX.sym('v_u'); omega_2_u = SX.sym('omega_2_u'); omega_3_u = SX.sym('omega_3_u');      % UAV cotrols
-v_t = SX.sym('v_t'); omega_t = SX.sym('omega_3');                                           % Parameters of target
+v_u = SX.sym('v_u'); omega_2_u = SX.sym('omega_2_u'); omega_3_u = SX.sym('omega_3_u');        % UAV cotrols parameters
+%v_t = SX.sym('v_t'); omega_t = SX.sym('omega_3');                                             % control Parameters of target
+
 controls = [v_u; omega_2_u; omega_3_u]; n_controls = length(controls);
 rhs = [v_u*cos(psi_u)*cos(theta_u); v_u*sin(psi_u)*cos(theta_u); v_u*sin(theta_u);...
-    omega_2_u; omega_3_u; v_t*cos(theta_t); v_t*sin(theta_t); omega_t];                     %systems r.h.s
+    omega_2_u; omega_3_u];                     %systems r.h.s
 
-f = Function('f',{states,controls}, {rhs});                                                 % Nonlinear Mapping Function f(x,u)
-U = SX.sym('U',n_controls, N);                                                              % Desition Variables 
-P = SX.sym('P',n_states + n_states);                                                       
-% This consists of initial states 0-7 and reference states 8-15
+f = Function('f',{states_u,controls}, {rhs});                                                 % Nonlinear Mapping Function f(x,u)
+U = SX.sym('U',n_controls, N);                                                                % Desition Variables 
+P = SX.sym('P',n_states_u + 2);                                                        
+% This consists of initial states od UAV 1-5 and reference states 6-7(reference states are target's states)
 
-X = SX.sym('X',n_states,(N+1));
-% Consists of future predicted states
+X = SX.sym('X',n_states_u,(N+1));
+% Consists of future predicted states of UAV
 
 %% Filling the defined sysem parameters --------------------------------------------------------------------------
 
-X(:,1) = P(1:8) % initial state
+X(:,1) = P(1:5) % initial state
 for k = 1:N
     st = X(:,k); con = U(:,k);
     f_value = f(st,con);
@@ -57,21 +60,56 @@ ff = Function('ff',{U,P},{X});
 obj = [];  % object function
 g = [];    % constrain vector
 
-w1 = 0.1; w2 = 1; % weighting coefficents each for one function w1 -- f1 and w2 -- f2
+w1 = 0.1; %w2 = 1; % weighting coefficents each for one function w1 -- f1 and w2 -- f2
 
-for k=1:N
-    
-    x_u = states(1:1);   y_u = states(2:1); x_t = states(6:1); y_t = states(7:1);
-    a = z_u*tan(VFOV/2); b = z_u*tan(HFOV/2);                                          % defining field of VFOV and HFOV is rest to done
-    A_k = (cos(psi)/a)^2 + (sin(psi)/b)^2 ; B_k = 2*cos(psi)*sin(psi)*(1/a^2 - 1/b^2) ; C_k = (sin(psi)/a)^2 + (cos(psi)/b)^2;  
-    obj = w1*sqrt((x_u - x_t)^2+(y_u - y_t)^2) + w2*((A_k*(x_u - x_t)^2 + B_k*(y_t - y_u)*(x_t - x_u) + C_k*(y_t - y_u)^2)-1); % J(X,U) objective function
+for k = 1:N
+    x = states_u(1:1); y = states_u(2:1);
+    %obj = obj + sqrt((x - target's x)^2 + (y - target's y)^2);
     
 end
 
+
+%% simulation of the target and UAV's distance ---------------------------------------------------------------------
+
+t = 0:T:10 %TIme Span
+
+% Initial Conditions
+x_u_0 = 90;
+y_u_0 = 150;     
+z_u_0 = 80;     % for UAV
+
+states_u(:,1) = [x_u_0; y_u_0;z_u_0;0;0];
+
+x_t_0 = 100;
+y_t_0 = 150;
+theta_t_0 = 0; % for target
+
+eta0 = [x_t_0; y_t_0; theta_t_0];
+
+states_t(:,1) = eta0;
+
+% Loop started from here
+
+for i=1:length(t)
     
+    shi = states_t(3,i); % Orientation of target
+    v_t = 12;  % linear velocity of target
+    omega_t = 1; % Angular velocity of target
+    z(1,i)=0; %target is on ground
     
+    eta_dot(:,i) = [v_t*cos(shi); v_t*sin(shi); omega_t];
     
+    states_t(:,i+1) = states_t(:,i) + T * eta_dot(:,i);
     
+    cost_value(1,i) = sqrt((x_u_0-states_t(1,i))^2 + (y_u_0-states_t(2,i))^2);
+end
+
+
+subplot(2,3,5) %plotting the cost values
+plot(t,cost_value(1,1:i));
+xlabel('T[s]');
+ylabel('cost value');
+grid on;
     
     
     
